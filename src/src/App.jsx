a09a8dbcc6f6d4e2f4b7d08418283145362e57e1
Nontuthuzelo Mtolo <217491599+NontuthuzeloM24@users.noreply.github.com>
2025-11-13 17:27:55 +0200

    update App.jsx with new components and improved layout
    - Integrated Header, PodcastCard, and Podcast components into App.jsx
    - Updated layout and styling for better UI structure
    - Cleaned up imports and improved code readability

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,20 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from "./components/Header"
-import Main from "./components/Main"
-import Footer from "./components/Footer"
+import { useEffect, useState } from "react";
+import PodcastGrid from "./components/PodcastGrid";
+import { genres } from "./data";
+import { fetchPodcasts } from "./api/fetchPodcasts";
+import Header from "./components/Header";
 
-function App() {
+/**
+ * App - The root component of the Podcast Explorer application. It handles:
+ * - Fetching podcast data from a remote API
+ * - Managing loading and error states
+ * - Rendering the podcast grid once data is successfully fetched
+ * - Displaying a header and fallback UI during loading or error
+ * @returns {JSX.Element} The rendered application interface
+ */
+
+export default function App() {
+  const [podcasts, setPodcasts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchPodcasts(setPodcasts, setError, setLoading);
+  }, []);
+
   return (
     <>
-      <Header name="Nontuthuzelo"
-              place="Cape Town" />
-      <Main />
-      <Footer />
-    </>
-  )
-}
+      <Header />
+      <main>
+        {loading && (
+          <div className="message-container">
+            <div className="spinner"></div>
+            <p>Loading podcasts...</p>
+          </div>
+        )}
 
-export default App
+        {error && (
+          <div className="message-container">
+            <div className="error">
+              Error occurred while tyring fetching podcasts: {error}
+            </div>
+          </div>
+        )}
+
+        {!loading && !error && (
+          <PodcastGrid podcasts={podcasts} genres={genres} />
+        )}
+      </main>
+    </>
+  );
+}
